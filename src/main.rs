1b13508ecb411d64385a69e2858e5df8602b4732
Manuel Amersdorfer <manuel.amersdorfer@kit.edu>
2024-03-12 00:35:23 +0100

    Adding license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Remove duplicated files found in a reference directory in a directory
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2024  Manuel Amersdorfer
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 use data_encoding::HEXLOWER;
 use log::{error, info};
 use log::{Level, LevelFilter, Metadata, Record};
